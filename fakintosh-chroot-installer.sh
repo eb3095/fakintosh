812af3f62f721f3aa081f18a188d11b659547ce9
Eric Benner <ebenner@choopa.com>
2019-06-09 04:12:50 -0400

    Fixes

--- fakintosh-chroot-installer.sh
+++ fakintosh-chroot-installer.sh
@@ -65,6 +65,21 @@ useradd -d /home/$user $user
 echo $user:"$userpw" | chpasswd
 chsh -s $(which zsh) $user
 chown -R $user:$user /home/$user
+usermod -aG wheel $user
+
+# Setup root
+cp /etc/skel/.zshrc /root/.zshrc
+chsh -s $(which zsh) root
+
+
+# Setup SUDOERS
+sed -i -e 's/# %wheel ALL=(ALL) NOPASSWD\: ALL/%wheelnpw ALL=(ALL) NOPASSWD\: ALL/' /etc/sudoers
+sed -i -e 's/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/' /etc/sudoers
+groupadd wheelnpw
+
+# Setup installer
+uderadd fakintosh
+usermod -aG wheelnpw
 
 # Install Grub
 if [ "$EFI" = true ] ; then
@@ -75,3 +90,35 @@ fi
 
 # Generate Grub
 grub-mkconfig -o /boot/grub/grub.cfg
+
+# Install trizen
+pushd /root/
+git clone https://aur.archlinux.org/trizen.git
+cd trizen
+makepkg -si
+cd ..
+rm -rf trizen
+popd
+
+# Install packages
+runuser -l fakintosh -c 'trizen -Sy --noconfirm weston plasma plasma-wayland-session kde-applications sddm opera pulseaudio tilix libreoffice-fresh kvantum-qt5 networkmanager nm-connection-editor network-manager-applet networkmanager-openvpn remmina notepadqq atom nvidia nvidia-settings thunderbird ufw vlc openssh nfs-utils bind-tools noto-fonts noto-fonts-extra noto-fonts-cjk noto-fonts-emoji numlockx screen nmap jq gotop iotop ccze htop expect sshuttle inkscape gimp jdk11-openjdk php sshfs ttf-ms-fonts kdeconnect ttf-dejavu ttf-liberation remmina-plugin-rdesktop plasma5-applets-kde-arch-update-notifier-git octopi filezilla opera-ffmpeg-codecs'
+
+# Enable services
+systemctl enable ufw
+systemctl enable sddm
+systemctl enable sshd
+systemctl enable NetworkManager
+systemctl enable dhcpcd
+
+
+# Configure Firewall
+ufw enable
+ufw default deny incoming
+ufw allow 22
+
+# Dispose of installer user
+userdel fakintosh
+
+# Cleanup
+rm /root/fakintosh-chroot-installer.sh
+rm /root/bootstrap.sh
