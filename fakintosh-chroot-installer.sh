5f6922804d4ffb47fc323072106eb37e43a05d34
Eric Benner <ebenner@choopa.com>
2019-06-09 02:02:41 -0400

    Initial

--- fakintosh-chroot-installer.sh
+++ fakintosh-chroot-installer.sh
@@ -4,22 +4,46 @@
 # Version: 1.0
 # Author: Eric Benner
 
+# Check for UEFI
+EFI=false
+EFIVARS=/sys/firmware/efi/efivars
+if [ -d "$EFI" ]; then
+    EFI=true
+fi
+
+# Set time zone
 ln -sf /usr/share/zoneinfo/America/New_York /etc/localtime
+
+# Set the clock
 hwclock --systohc
+
+# Configure locale
 sed -i -e "s/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/" /etc/locale.gen
+
+# Generate locale
 locale-gen
+
+# Configure locale
 echo LANG=en_US.UTF-8 > /etc/locale.conf
+
+# Set hostname
 echo iMac > /etc/hostname
+
+# Create Hosts file
 echo "127.0.0.1      localhost" >> /etc/hosts
 echo "::1            localhost" >> /etc/hosts
 echo "127.0.1.1      iMac.localdomain iMac" >> /etc/hosts
+
+# Make initramfs
 mkinitcpio -p linux
 
+# Install ABSOLUTE essentials
 pacman -Sy wget git unzip zip base-devel grub zsh --noconfirm
 
 echo "Enter a root password [ENTER]:"
 read rootpw
 
+# Set root password
 echo $rootpw | passwd root --stdin
 
 echo "Enter a user [ENTER]:"
@@ -28,14 +52,20 @@ read user
 echo "Enter a $user's password [ENTER]:"
 read userpw
 
+# Setup user
 mkdir /home/$user
 cp /etc/skel/.zshrc /home/$user/.zshrc
 useradd -d /home/$user $user
 echo $userpw | passwd $user --stdin
 chsh -s $(which zsh) $user
+chown -R $user:$user /home/$user
 
-# BIOS
-# grub-install --target=i386-pc /dev/vda
+# Install Grub
+if [ "$EFI" = true ] ; then
+  grub-install --target=x86_64-efi --efi-directory=esp --bootloader-id=GRUB
+else
+  grub-install --target=i386-pc /dev/vda
+fi
 
-# GPT
-# grub-install --target=x86_64-efi --efi-directory=esp --bootloader-id=GRUB
+# Generate Grub
+grub-mkconfig -o /boot/grub/grub.cfg
--- fakintosh.sh
+++ fakintosh.sh
@@ -4,8 +4,10 @@
 # Version: 1.0
 # Author: Eric Benner
 
+# Set the time and date
 timedatectl set-ntp true
 
+# Check for UEFI
 EFI=false
 EFIVARS=/sys/firmware/efi/efivars
 if [ -d "$EFI" ]; then
@@ -16,6 +18,7 @@ echo "WARNING: THIS WILL WIPE THE DRIVE"
 echo "What drive do you want to install to? (/dev/sda) [ENTER]:"
 read DRIVE
 
+# Partition drive
 if [ "$EFI" = true ] ; then
   parted --script $drive mklabel gpt
   parted --script $drive mkpart primary fat32 1MiB 261MiB
@@ -32,11 +35,13 @@ else
   mkfs.ext4 "$drive"1
 fi
 
+# Format drive
 mkswap "$drive"2
 swapon "$drive"2
 mkfs.ext4 "$drive"3
 mount "$drive"3 /mnt
 
+# Mount drive
 if [ "$EFI" = true ] ; then
   mkdir /mnt/efi
   mount "$drive"1 /mnt/efi
@@ -45,10 +50,15 @@ else
   mount "$drive"1 /mnt/boot
 fi
 
+# Install base
 pacstrap /mnt base
+
+# Generate fstab
 genfstab -U /mnt >> /mnt/etc/fstab
 
-# Get second part of the installer
-wget 
+# Download script
+wget https://raw.githubusercontent.com/eb3095/fakintosh/master/fakintosh-chroot-installer.sh
 mv fakintosh-chroot-installer.sh /mnt/root/fakintosh-chroot-installer.sh
+
+# Chroot in and run second part
 arch-chroot /mnt /mnt/root/fakintosh-chroot-installer.sh
